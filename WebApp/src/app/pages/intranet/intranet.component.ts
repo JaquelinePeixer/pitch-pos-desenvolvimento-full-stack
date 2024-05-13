f4a92cae754ab524844cd6fa2bd8b9ccf86a7be8
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2024-05-13 06:25:07 -0300

    started frontend

---
+++ WebApp/src/app/pages/intranet/intranet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intranet',
+  standalone: true,
+  imports: [],
+  templateUrl: './intranet.component.html',
+  styleUrl: './intranet.component.scss'
+})
+export class IntranetComponent {
+
+}
