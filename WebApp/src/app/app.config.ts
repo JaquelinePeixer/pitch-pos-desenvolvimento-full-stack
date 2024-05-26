0f27ed6bb5202737646e933bab7f3f3120a2c0d4
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2024-05-26 10:34:58 -0300

    translate add

--- WebApp/src/app/app.config.ts
+++ WebApp/src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+// required for AoT 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'pt',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      HttpClientModule,
+      TranslateModule.forRoot(provideTranslation())
+    ]),
+  ]
 };
