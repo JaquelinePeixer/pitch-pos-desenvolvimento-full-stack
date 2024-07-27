755282dcb41a060b4a6155e2e29d2e92d2ef5c20
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2024-07-27 06:12:49 -0300

    Page not found

--- WebApp/src/app/pages/page-not-found/page-not-found.component.ts
+++ WebApp/src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [SharedModule],
+  imports: [RouterLink, SharedModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
